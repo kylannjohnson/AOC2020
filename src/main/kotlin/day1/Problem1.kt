bb8b4cbe31de182dbe2697aea18d60c255a4503c
Kylan <kylan@iMac.local>
2020-12-01 23:08:46 -0700

    Completed 1.2

--- src/main/kotlin/day1/Problem1.kt
+++ src/main/kotlin/day1/Problem1.kt
@@ -2,20 +2,29 @@ package day1
 
 import util.getResourceAsText
 
+val num = listOf(
+    1721,
+    979,
+    366,
+    299,
+    675,
+    1456
+)
 
 fun main() {
-    val num = listOf(
-        1721,
-        979,
-        366,
-        299,
-        675,
-        1456
-    )
 
     val target = 2020
     val nums = getResourceAsText("data1.txt").map { it.toInt() }
-    println(pairOfTarget(nums, target))
+//    println(pairOfTarget(nums, target))
+    val triples = mutableListOf<List<Int>>()
+    tripleOfTarget(nums, 3, mutableListOf(), triples)
+
+    val (f, s, t) = triples
+        .map { it to it.sum() }
+        .first { it.second == target }
+        .first
+
+    println("${f * s * t}")
 }
 
 
@@ -26,3 +35,27 @@ fun pairOfTarget(num: List<Int>, target: Int): Int {
 
     return first * second
 }
+
+// https://hmkcode.com/calculate-find-all-possible-combinations-of-an-array-using-java/
+fun tripleOfTarget(num: List<Int>, numberOfSpacesLeft: Int, group: List<Int>, triples: MutableList<List<Int>>) {
+
+
+    // if num.length < k then stop
+    if (num.size < numberOfSpacesLeft)
+        return
+
+    //  if k == 1 then add each element to the accumulated combination
+    if (numberOfSpacesLeft == 1) {
+        num.forEach { triples.add(group + it) }
+    }
+    //  if num.length == k then add all elements to the accumulated combination.
+    else if (num.size == numberOfSpacesLeft) {
+        triples.add(group + num)
+    }
+    // if num.length > k then for each element e make a recursive call passing sub list of the elements list, k-1 and add element e to accumulated combination.
+    else if (num.size > numberOfSpacesLeft) {
+        for (i in num.indices) tripleOfTarget(num.subList(i + 1, num.size), numberOfSpacesLeft - 1, (group + num[i]), triples)
+    }
+}
+
+
