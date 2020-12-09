c2fbdff63e7b039bfeee6ddad80010240d84f4b9
kylan <k.johnson.az@gmail.com>
2020-12-08 22:57:17 -0700

    day 7.2 done

--- src/main/kotlin/day7/Problem.kt
+++ src/main/kotlin/day7/Problem.kt
@@ -4,7 +4,7 @@ import util.getResourceAsText
 
 fun main() {
 
-    val rules = listOf(
+    val rules1 = listOf(
         "light red bags contain 1 bright white bag, 2 muted yellow bags.",
         "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
         "bright white bags contain 1 shiny gold bag.",
@@ -16,18 +16,42 @@ fun main() {
         "dotted black bags contain no other bags."
     )
 
-//    val rulesList = rules.toRulesList()
+    val rules2 = listOf(
+        "shiny gold bags contain 2 dark red bags.",
+        "dark red bags contain 2 dark orange bags.",
+        "dark orange bags contain 2 dark yellow bags.",
+        "dark yellow bags contain 2 dark green bags.",
+        "dark green bags contain 2 dark blue bags.",
+        "dark blue bags contain 2 dark violet bags.",
+        "dark violet bags contain no other bags.",
+    )
+
+//    val rulesList = rules2.toRulesList()
     val rulesList = getResourceAsText("data7.txt").toRulesList()
 
     println(findBags(rulesList, listOf("shiny gold"), emptySet()))
-
-//    rulesList.forEach(::println)
+    println(findTotalBags(rulesList, 1 to "shiny gold") - 1)
 }
 
 fun findBags(rulesList: List<Rule>, targetBags: List<String>, currentCargo: Set<String>): Int {
-    val found = rulesList.filter { bag -> targetBags.any { it in bag.canContain } }.map { it.containingBagColor }
+    val found =
+        rulesList.filter { bag -> targetBags.any { it in bag.canContain.map { c -> c.second } } }
+            .map { it.containingBagColor }
 
     return if (found.isEmpty()) currentCargo.size else findBags(rulesList, found, currentCargo + found)
+}
+
+fun findTotalBags(rulesList: List<Rule>, targetBags: Pair<Int, String>): Int {
+    val found: List<Pair<Int, String>> =
+        rulesList.first { bag -> bag.containingBagColor == targetBags.second }.canContain
+
+    return if (found.isEmpty()) {
+        targetBags.first
+    } else {
+        targetBags.first + targetBags.first * found.fold(0) { acc, pair ->
+            acc + findTotalBags(rulesList, pair)
+        }
+    }
 }
 
 // H/t to this snippet to simplify this https://github.com/brentwatson/AOC2020/blob/main/src/day7.ws.kts
@@ -37,11 +61,12 @@ fun List<String>.toRulesList() = map { rule ->
         containingBagColor = color,
         canContain = amountAndCargo
             .split(", ")
-            .map {
-                it.replace("\\d ".toRegex(), "")
-                    .replace(" bag(.*)".toRegex(), "")
+            .mapNotNull {
+                "(\\d)(.*) bag(.*)".toRegex().find(it)?.groupValues?.let { g ->
+                    g[1].toInt() to g[2].trim()
+                }
             }
     )
 }
 
-data class Rule(val containingBagColor: String, val canContain: List<String>)
+data class Rule(val containingBagColor: String, val canContain: List<Pair<Int, String>>)
